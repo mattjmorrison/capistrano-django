b1edfd36c1bb688f80ed99c58dcf17bbc83a2452
Matthew J. Morrison <mattjmorrison@mattjmorrison.com>
2013-01-27 14:36:23 -0600

    initial

---
+++ capistrano-django.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+
+  s.name     = "capistrano-django"
+  s.version  = "0.0.1"
+
+  s.homepage = "http://github.com/mattjmorrison/django-capistrano"
+  s.summary  = %q{capistrano-django - Welcome to easy deployment with Ruby over SSH for Django}
+  s.description = %q{capistrano-django provides a solid basis for common django deployment}
+
+  s.files    = Dir["lib/**/*.rb"]
+  s.add_dependency "capistrano", "~> 2.14.1"
+
+  s.author   = "Matthew J. Morrison"
+  s.email    = "mattjmorrison@mattjmorrison.com"
+
+end
---
+++ lib/capistrano/django.rb
@@ -0,0 +1,81 @@
+
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "capistrano_django requires Capistrano 2"
+end
+
+Capistrano::Configuration.instance.load do
+
+  set :normalize_asset_timestamps, false
+
+  after 'deploy:update', 'deploy:cleanup'
+  after 'deploy:update_code', 'nodejs:install_deps'
+  after 'deploy:update_code', 'python:create_virtualenv'
+  after 'python:create_virtualenv', 'python:install_deps'
+  after 'python:install_deps', 'django:compress'
+  after 'django:compress', 'django:collectstatic'
+  after 'django:collectstatic', 'django:symlink_settings'
+  before 'deploy:create_symlink', 'django:migrate'
+
+  namespace :nodejs do
+
+    desc "Install node.js dependencies"
+    task :install_deps do
+      run "cd #{current_release}/devops && npm install"
+    end
+
+  end
+
+  namespace :python do
+
+    desc "Create a python virtualenv"
+    task :create_virtualenv do
+      run "virtualenv #{current_release}/virtualenv"
+    end
+
+    desc "Install python requirements"
+    task :install_deps do
+      pip = "#{current_release}/virtualenv/bin/pip"
+      run "#{pip} install -r #{current_release}/devops/requirements/#{django_env}.txt"
+    end
+
+  end
+
+  namespace :django do
+
+    def django(args, flags="")
+      python = "#{current_release}/virtualenv/bin/python"
+      run "#{python} #{current_release}/manage.py #{django_env} #{args} #{flags}"
+    end
+
+    desc "Run django-compressor"
+    task :compress do
+      django("compress")
+    end
+
+    desc "Run django's collectstatic"
+    task :collectstatic do
+      django("collectstatic", "-i *.coffee -i *.less --noinput")
+    end
+
+    desc "Run django migrations"
+    task :migrate do
+      django("syncdb", "--noinput --migrate")
+    end
+
+    desc "Symlink django settings to deployed.py"
+    task :symlink_settings do
+      run "ln -s #{current_release}/project/settings/#{django_env}.py #{current_release}/project/settings/deployed.py"
+    end
+
+  end
+
+  namespace :deploy do
+
+    desc "Restart apache"
+    task :restart do
+      run "sudo apache2ctl graceful"
+    end
+
+  end
+
+end
