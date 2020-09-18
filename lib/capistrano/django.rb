85a3cd7ebe46ce40eafa7a647e8a3c7b0b4ea734
Agus Mathews <agus@tentwentyfour.lu>
2020-09-18 12:57:19 +0200

    Fixed compilemessages execution by executing inside path

--- lib/capistrano/django.rb
+++ lib/capistrano/django.rb
@@ -73,7 +73,9 @@ namespace :django do
   def django(args, flags="", run_on=:all)
     on release_roles(run_on) do |h|
       manage_path = File.join(release_path, fetch(:django_project_dir) || '', 'manage.py')
-      execute "#{release_path}/virtualenv/bin/python", "#{manage_path}", "#{fetch(:django_settings)}", "#{args}", "#{flags}"
+      within release_path do
+        execute "./virtualenv/bin/python", "#{manage_path}", "#{args}", "#{flags}"
+      end
     end
   end
 
